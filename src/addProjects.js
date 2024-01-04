0cd2698c5c56d498580dab4eb9e061fa6883210d
AdleenaLobo <1069455+AdleenaLobo@users.noreply.github.com>
2024-01-04 21:55:15 +0530

    Complete contact section

---
+++ src/addProjects.js
@@ -0,0 +1,6 @@
+import { library } from "../assets/library page.png";
+import { restraurant } from "../assets/restraurant page.png";
+import { todo } from "../assets/todo.png";
+import { weather } from "../assets/weather app.png";
+
+const array = [restraurant, library, weather, todo];
