299c816e19293f71cb68a34628e8c87b6fd8eba5
AdleenaLobo <1069455+AdleenaLobo@users.noreply.github.com>
2024-08-05 11:37:28 +0530

    Finished update v1.0

--- src/addProjects.js
+++ src/addProjects.js
@@ -1,79 +1,79 @@
 import library from "../assets/library page.png";
-import restraurant from "../assets/restraurant page.png";
+import TicTacToe from "../assets/tic-tac-toe.png";
 import todo from "../assets/todo.png";
 import weather from "../assets/weather app.png";
 
-const array = ["restraurant", "library", "weather", "todo"];
-const array1 = [1, 0, 1, 0];
+const array = ["library", "weather", "todo", "tic-tac-toe"];
 let outerdiv;
-let innerElements1, innerElements2;
+let innerElements1, innerElements2, header;
 let link;
-let wrapText;
 
 function startProcess() {
-  for (let i = 0; i < array1.length; i++) {
-    createDivs(array1[i], array[i]);
+  for (let i = 0; i < array.length; i++) {
+    createDivs( array[i]);
   }
 }
-function createDivs(varA, varB) {
+function createDivs(varB) {
   outerdiv = document.createElement("div");
   link = document.createElement("a");
+  innerElements1 = document.createElement("img");
+  innerElements2 = document.createElement("div");
+  header = document.createElement("div");
   outerdiv.id = "project-content";
-  wrapText = document.createElement("div");
-  wrapText.id = "project-content-text";
-  if (varA == 1) {
-    innerElements1 = document.createElement("img");
-    innerElements2 = document.createElement("div");
-    if (varB == "restraurant") {
-      link.href = "https://adleenalobo.github.io/Restaurant-Page/";
-      innerElements1.src = restraurant;
+  innerElements2.id="project-content-text";
+
+    if (varB == "tic-tac-toe") {
+      link.href = "https://adleenalobo.github.io/tic_tac_toe/dist/index.html";
+      innerElements1.setAttribute("src", TicTacToe);
       innerElements2.textContent =
-        "A restraurant site which has 3 main pages : About us page , Menu and Contact information. Inside each page the content is in pluckcard format. ";
-      addElemToHtml(link, wrapText, outerdiv, innerElements1, innerElements2);
+        "tic tac toe game website built. In this game the user has 2 options , 1.To play with a human opponent and 2.To play with an AI.";
+      header.textContent = "TIC-TAC-TOE";
+      header.classList.add("projects-header");
+      addElemToHtml(link, outerdiv, innerElements1, innerElements2,header);
+
     } else if (varB == "weather") {
       link.href = "https://adleenalobo.github.io/weather_app/dist/index";
       innerElements1.setAttribute("src", weather);
       innerElements2.textContent =
         "A weather forecasting app  , here one can search a city and the  current weather will be displayed.One can toggle the temperature to farenhiet or celsius and that state saved so even when one reloads the page it will be on the last toggled mode.This website has been built using weather api.";
-      addElemToHtml(link, wrapText, outerdiv, innerElements1, innerElements2);
+        header.textContent = "WEATHER";
+        header.classList.add("projects-header");
+      addElemToHtml(link, outerdiv, innerElements1, innerElements2,header);
+
     }
-  } else {
-    innerElements2 = document.createElement("img");
-    innerElements1 = document.createElement("div");
-    if (varB == "library") {
+    else if (varB == "library") {
       link.href = "https://adleenalobo.github.io/Odin-Library/";
-      innerElements2.setAttribute("src", library);
-      innerElements1.textContent =
+      innerElements1.setAttribute("src", library);
+      innerElements2.textContent =
         "A library page , it has a feature of adding books, once clicked on  + a form will pop up to fill in the details, one can delete the books too";
-      addElemToHtml(link, wrapText, outerdiv, innerElements1, innerElements2);
+        header.textContent = "LIBRARY";
+        header.classList.add("projects-header");
+      addElemToHtml(link, outerdiv, innerElements1, innerElements2,header);
+
     } else if (varB == "todo") {
       link.href = "https://adleenalobo.github.io/todo_list/dist/index.html";
-      innerElements2.setAttribute("src", todo);
-      innerElements1.textContent =
+      innerElements1.setAttribute("src", todo);
+      innerElements2.textContent =
         "A ToDo list website , here we have 2 main pages : About , Home. When clicked on home we get the list of the todo's that we have entered,  we can add todo's by clicking on + symbol . This project has been built using date-fns . It also has toggle option for theme and saves the todo's and the state even after a reload .";
-      addElemToHtml(link, wrapText, outerdiv, innerElements1, innerElements2);
-    }
-  }
+        header.textContent = "TODO";
+        header.classList.add("projects-header");
+      addElemToHtml(link, outerdiv, innerElements1, innerElements2,header);
+    }  
 }
+
 function addElemToHtml(
   link,
-  wrapText,
   outerdiv,
   innerElements1,
-  innerElements2
+  innerElements2,
+  header
 ) {
   let project = document.getElementById("projects");
-  if (innerElements1.src == undefined) {
-    wrapText.appendChild(innerElements1);
-    outerdiv.appendChild(wrapText);
-    outerdiv.appendChild(innerElements2);
-  } else {
-    outerdiv.appendChild(innerElements1);
-    wrapText.appendChild(innerElements2);
-    outerdiv.appendChild(wrapText);
-  }
-  project.appendChild(link);
-  link.appendChild(outerdiv);
+  link.appendChild(innerElements1);
+  link.appendChild(header);
+  outerdiv.appendChild(link);
+  outerdiv.appendChild(innerElements2);
+  project.appendChild(outerdiv);
 }
 
 export { startProcess };
